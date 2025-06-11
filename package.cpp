934d970b9273c23cf9a2419121ee2507ae4e3962
gonzales-aj <gonzales-aj@outlook.com>
2025-06-10 19:32:37 -0700

    Add files via upload

---
+++ package.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <typeinfo>
+
+using namespace std;
+
+template<typename T>
+class Package {
+private:
+    T item;
+public:
+    Package(T i) : item(i) {}
+    void label() {
+        std::cout << "Generic package containing: " << typeid(T).name() << "\n";
+    }
+};
+
+// Specialization for std::string
+template<>
+class Package<std::string> {
+private:
+    std::string item;
+public:
+    Package(std::string i) : item(i) {}
+    void label() {
+        std::cout << "Book package: \"" << item << "\"\n";
+    }
+};
+
+// Partial specialization for pointers
+template<typename T>
+class Package<T*> {
+private:
+    T* item;
+public:
+    Package(T* i) : item(i) {}
+    void label() {
+        std::cout << "Fragile package for pointer to type: " << typeid(T).name() << "\n";
+    }
+};
+
+template<typename T, int Size>
+class Box {
+private:
+    std::array<T, Size> items;
+    int count = 0;
+public:
+    bool addItem(const T& item) {
+        if (count < Size) {
+            items[count++] = item;
+            std::cout << "Added item to box: \"" << item << "\"\n";
+            return true;
+        }
+        return false;
+    }
+
+    void printItems() const {
+        std::cout << "Box contents:\n";
+        for (int i = 0; i < count; ++i) {
+            std::cout << " - " << items[i] << "\n";
+        }
+    }
+};
+
+template<typename T>
+void shipItem(const T& item) {
+    std::cout << "Shipping item of type: " << typeid(T).name() << "\n";
+}
+
+template<>
+void shipItem(const double& item) {
+    std::cout << "Shipping temperature-controlled item: " << item << "°C\n";
+}
+
+int main() {
+    Package<int> p1(42);
+    p1.label();  // Generic package
+
+    Package<std::string> p2("C++ Primer");
+    p2.label();  // Book package
+
+    double d = 9.81;
+    Package<double*> p3(&d);
+    p3.label();  // Fragile package
+
+    // Box example
+    Box<std::string, 3> bookBox;
+    bookBox.addItem("The Pragmatic Programmer");
+    bookBox.addItem("Clean Code");
+    bookBox.printItems();
+
+    // Shipping examples
+    shipItem(7);
+    shipItem(22.5);
+
+    return 0;
+}
